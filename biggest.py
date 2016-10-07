fffc203ba8e478824328e6e7f5fd1ae74a8f0377
alexander ridley <aridley19@VenerableBede.schools.sunderland.gov.uk>
2016-10-07 10:25:35 +0100

    Fixed 'again'

--- biggest.py
+++ biggest.py
@@ -26,14 +26,13 @@ def run_tests():
 # Edit this code
 def biggest(a, b, c):
 
-    if a > b and a > c:
+    if a >= b and a >= c:
        print (a)
 
-    elif b > a and b > c:
+    elif b >= a and b >= c:
          print (b)
 
-    else: 
-         print (c)
-         
-         
+    elif c >= a and c >= b:
+        print(c)
+
 
